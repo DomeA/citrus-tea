7f4cf8edfdd2e58f007ffaf8ef1ab5a217147475
DomeA <domeastudio@gmail.com>
2017-12-10 00:35:13 +0800

    surveying init

---
+++ servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/domain/postgresql/services/DhtmlxService.java
@@ -0,0 +1,7 @@
+package com.domeastudio.mappingo.servers.microservice.surveying.domain.postgresql.services;
+
+import com.domeastudio.mappingo.servers.microservice.surveying.dto.response.DhtmlxSidebar;
+
+public interface DhtmlxService {
+    DhtmlxSidebar getDhtmlxSidebarData();
+}
---
+++ servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/domain/postgresql/services/impl/DhtmlxServiceImpl.java
@@ -0,0 +1,12 @@
+package com.domeastudio.mappingo.servers.microservice.surveying.domain.postgresql.services.impl;
+
+import com.domeastudio.mappingo.servers.microservice.surveying.domain.postgresql.services.DhtmlxService;
+import com.domeastudio.mappingo.servers.microservice.surveying.dto.response.DhtmlxSidebar;
+
+public class DhtmlxServiceImpl implements DhtmlxService {
+    @Override
+    public DhtmlxSidebar getDhtmlxSidebarData() {
+
+        return null;
+    }
+}
