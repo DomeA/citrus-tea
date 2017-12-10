906396b14cfbac474bedea586e08392b18eb2d4d
DomeA <domeastudio@gmail.com>
2017-12-10 14:27:23 +0800

    surveying init

--- servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/rest/DhtmlXAPI.java
+++ servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/rest/DhtmlXAPI.java
@@ -1,7 +1,11 @@
 package com.domeastudio.mappingo.servers.microservice.surveying.rest;
 
+import com.domeastudio.mappingo.servers.microservice.surveying.domain.postgresql.services.DhtmlxService;
 import com.domeastudio.mappingo.servers.microservice.surveying.dto.response.ClientMessage;
+import com.domeastudio.mappingo.servers.microservice.surveying.dto.response.DhtmlxData;
+import com.domeastudio.mappingo.servers.microservice.surveying.dto.response.ResultStatusCode;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/manager/menu")
 public class DhtmlXAPI {
+    @Autowired
+    DhtmlxService dhtmlxService;
 
     @RequestMapping(value = "sildebar/{roleName}")
     public ClientMessage getSidebarData(@PathVariable("roleName") String roleName){
-
-        return null;
+        DhtmlxData dhtmlxData = dhtmlxService.getDhtmlxSidebarData(roleName);
+        ClientMessage clientMessage;
+        if(dhtmlxData.getItems().size()>0){
+            clientMessage = new ClientMessage(ResultStatusCode.OK.getCode(),
+                    ResultStatusCode.OK.getMsg(), dhtmlxData);
+            return clientMessage;
+        }
+        clientMessage = new ClientMessage(ResultStatusCode.INVALID_PARAM.getCode(),
+                ResultStatusCode.INVALID_PARAM.getMsg(), null);
+        return clientMessage;
     }
 }
