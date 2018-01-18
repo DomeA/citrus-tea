b9c699c163cf3e4a1b6441655607a78bc19c36bf
DomeA <domeastudio@gmail.com>
2018-01-18 16:52:11 +0800

    surveying init

--- servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/domain/postgresql/repository/BbuildingProcessformRepository.java
+++ servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/domain/postgresql/repository/BbuildingProcessformRepository.java
@@ -1,9 +1,9 @@
 package com.domeastudio.mappingo.servers.microservice.surveying.domain.postgresql.repository;
 
-import com.domeastudio.mappingo.servers.microservice.surveying.domain.postgresql.pojo.BprocessformEntity;
+import com.domeastudio.mappingo.servers.microservice.surveying.domain.postgresql.pojo.BbuildingProcessformEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BprocessformRepository extends JpaRepository<BprocessformEntity,String> {
+public interface BbuildingProcessformRepository extends JpaRepository<BbuildingProcessformEntity,String> {
 }
---
+++ servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/domain/postgresql/repository/BcadastralProcessformRepository.java
@@ -0,0 +1,4 @@
+package com.domeastudio.mappingo.servers.microservice.surveying.domain.postgresql.repository;
+
+public interface BcadastralProcessformRepository {
+}
