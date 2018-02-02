88aeeaca539800abaa6f29568302339ad9b1f193
DomeA <domeastudio@gmail.com>
2018-02-02 19:14:05 +0800

    surveying init

--- servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/domain/postgresql/services/impl/BusinessServiceImpl.java
+++ servers/micro-service/surveying/src/main/java/com/domeastudio/mappingo/servers/microservice/surveying/domain/postgresql/services/impl/BusinessServiceImpl.java
@@ -38,63 +38,63 @@ public class BusinessServiceImpl implements BusinessService {
 
     @Override
     public Object newBuinessData(ProcessType processType) {
-        Object obj=null;
-        switch (processType){
+        Object obj = null;
+        switch (processType) {
             case SURVEY:
-                obj=newSurveyProcess();
+                obj = newSurveyProcess();
                 break;
             case BUILDING:
-                newBuildingProcess();
+                obj = newBuildingProcess();
                 break;
             case CADASTRAL:
-                newCadastralProcess();
+                obj = newCadastralProcess();
                 break;
         }
         return obj;
     }
 
-    private BsurveyProcessformEntity newSurveyProcess(){
-        BsurveyProcessformEntity bsurveyProcessformEntity=new BsurveyProcessformEntity();
+    private BsurveyProcessformEntity newSurveyProcess() {
+        BsurveyProcessformEntity bsurveyProcessformEntity = new BsurveyProcessformEntity();
 
-        SurveyProjectEntity surveyProjectEntity=new SurveyProjectEntity();
+        SurveyProjectEntity surveyProjectEntity = new SurveyProjectEntity();
         surveyProjectEntity = surveyProjectRepository.save(surveyProjectEntity);
         bsurveyProcessformEntity.setProjectAchievementsId(surveyProjectEntity.getId());
 
-        DatumEntity datumEntity=new DatumEntity();
-        datumEntity=datumRepository.save(datumEntity);
+        DatumEntity datumEntity = new DatumEntity();
+        datumEntity = datumRepository.save(datumEntity);
         bsurveyProcessformEntity.setDatumId(datumEntity.getId());
 
         return bsurveyProcessformRepository.save(bsurveyProcessformEntity);
     }
 
-    private BcadastralProcessformEntity newCadastralProcess(){
-        BcadastralProcessformEntity bcadastralProcessformEntity=new BcadastralProcessformEntity();
+    private BcadastralProcessformEntity newCadastralProcess() {
+        BcadastralProcessformEntity bcadastralProcessformEntity = new BcadastralProcessformEntity();
 
-        CadastralProjectEntity cadastralProjectEntity=new CadastralProjectEntity();
+        CadastralProjectEntity cadastralProjectEntity = new CadastralProjectEntity();
         cadastralProjectEntity = cadastralProjectRepository.save(cadastralProjectEntity);
         bcadastralProcessformEntity.setProjectAchievementsId(cadastralProjectEntity.getId());
 
-        DatumEntity datumEntity=new DatumEntity();
-        datumEntity=datumRepository.save(datumEntity);
+        DatumEntity datumEntity = new DatumEntity();
+        datumEntity = datumRepository.save(datumEntity);
         bcadastralProcessformEntity.setDatumId(datumEntity.getId());
 
         return bcadastralProcessformRepository.save(bcadastralProcessformEntity);
     }
 
-    private BbuildingProcessformEntity newBuildingProcess(){
-        BbuildingProcessformEntity bbuildingProcessformEntity=new BbuildingProcessformEntity();
+    private BbuildingProcessformEntity newBuildingProcess() {
+        BbuildingProcessformEntity bbuildingProcessformEntity = new BbuildingProcessformEntity();
 
 //        BuildingHouseholdEntity buildingHouseholdEntity=new BuildingHouseholdEntity();
 //        buildingHouseholdEntity=buildingHouseholdRepository.save(buildingHouseholdEntity);
 //
 //        BuildingLayerEntity  buildingLayerEntity=new BuildingLayerEntity();
 //        buildingLayerEntity.set
-        BuildingProjectEntity buildingProjectEntity=new BuildingProjectEntity();
+        BuildingProjectEntity buildingProjectEntity = new BuildingProjectEntity();
         buildingProjectEntity = buildingProjectRepository.save(buildingProjectEntity);
         bbuildingProcessformEntity.setProjectAchievementsId(buildingProjectEntity.getId());
 
-        DatumEntity datumEntity=new DatumEntity();
-        datumEntity=datumRepository.save(datumEntity);
+        DatumEntity datumEntity = new DatumEntity();
+        datumEntity = datumRepository.save(datumEntity);
         bbuildingProcessformEntity.setDatumId(datumEntity.getId());
 
         return bbuildingProcessformRepository.save(bbuildingProcessformEntity);
